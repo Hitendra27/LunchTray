a98af143e72806e4c8653535fc72f4028ce18f53
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-16 13:36:09 +0100

    feat: Implemented NavHost in LunchTrayScreen.kt

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,12 +16,15 @@
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
 import com.example.lunchtray.ui.OrderViewModel
 
 // TODO: Screen enum
@@ -51,5 +54,13 @@ fun LunchTrayApp() {
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+            builder = {},
+            modifier =  Modifier.padding(innerPadding)
+        ) {
+
+        }
     }
 }
