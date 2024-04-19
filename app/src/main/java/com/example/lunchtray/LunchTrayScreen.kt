b73f7d5a1c10567c1e6d852908e20bdccb38670e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-19 21:22:24 +0100

    feat: Implemented Navigation controller and initialization

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,6 +16,8 @@
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -23,9 +25,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.ui.EntreeMenuScreen
 import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.ui.StartOrderScreen
 
 // TODO: Screen enum
 enum class LunchTrayScreen(@StringRes val title: Int) {
@@ -42,6 +52,14 @@ enum class LunchTrayScreen(@StringRes val title: Int) {
 @Composable
 fun LunchTrayApp() {
     // TODO: Create Controller and initialization
+    // Create NavController
+    val navController = rememberNavController()
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.Start.name
+    )
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
@@ -60,7 +78,38 @@ fun LunchTrayApp() {
             builder = {},
             modifier =  Modifier.padding(innerPadding)
         ) {
+            composable(route = LunchTrayScreen.Start.name) {
+                StartOrderScreen(
+                    onStartOrderButtonClicked = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.padding_medium))
+                )
+                composable(route = LunchTrayScreen.Entree.name) {
+                    EntreeMenuScreen(
+                        options = DataSource.entreeMenuItems,
+                        onCancelButtonClicked = {
+                                                viewModel.resetOrder()
+                        },
+                        onNextButtonClicked = { /*TODO*/ },
+                        onSelectionChanged = 
+                    )
+                }
+                composable(route = LunchTrayScreen.Start) {
+                }
+                composable(route = LunchTrayScreen.Side) {
+
+                }
+                composable(route = LunchTrayScreen.Accompaniment) {
+
+                }
+                composable(route = LunchTrayScreen.Checkout) {
+                    
+                }
+            }
 
         }
+
     }
 }
+
